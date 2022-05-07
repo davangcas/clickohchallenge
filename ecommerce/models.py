60a37af0496d073bab32d3869200ea32bb933f71
Angel David Castro <davangcas@gmail.com>
2022-05-07 12:52:04 -0300

    feat: create models

---
+++ ecommerce/models.py
@@ -0,0 +1,18 @@
+from pyexpat import model
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+
+
+class Order(models.Model):
+    date_time = models.DateTimeField()
+
+
+class OrderDetail(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
+    quantity = models.PositiveIntegerField()
